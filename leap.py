38af50e7734297446aeda55e7c8ff1795611c0c7
Chavi99 <44029954+Chavi99@users.noreply.github.com>
2019-02-17 05:35:43 -0800

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,5 +1,7 @@
-year=int(input())
-if year%4==0 and (year%400==0 or year%100!=0):
-    print("Yes")
-else:
-    print("No")
+def leap(a):
+    if a % 4 == 0 and (a % 400 == 0 or a % 100 != 0):
+        print("leap year")
+    else:
+        print("not")
+a=int(input())
+leap(a)
